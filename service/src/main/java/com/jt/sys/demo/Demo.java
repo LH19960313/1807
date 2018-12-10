7c57e9bc4f113d237463e3123d653349d10e8faa
LH19960313 <2953300934@qq.com>
2018-12-10 13:56:32 +0800

    2018/12/11/001

--- service/src/main/java/com/jt/sys/demo/Demo.java
+++ service/src/main/java/com/jt/sys/demo/Demo.java
@@ -32,14 +32,8 @@ public class Demo {
     JsonResult Check( @RequestBody  Map mao) {
         System.out.println( mao);
         Map map=new HashMap<>();
-        map.put("c",dao.finnews());
-
-
-
+        map.put("sss",dao.finnews());
         return new JsonResult(map);
-
-
-
     }
 
     @PostMapping(value = "test" )
